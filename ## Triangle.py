9874523c895a3067741ec02e1a919c2d52ccad0a
Regie Agustin <124678791+miinks@users.noreply.github.com>
2024-03-19 23:09:30 -0500

    testing how to make triangle

--- ## Triangle.py
+++ ## Triangle.py
@@ -1,7 +1,46 @@
-## Triangle 
 
-def print_triangle(height):
-    for i in range(1, height + 1 ):
-        print('*' * i )
 
-print_triangle(5)
+# Function to print full pyramid pattern
+def full_pyramid(n):
+    for i in range(1, n + 1):
+        # Print leading spaces
+        for j in range(n - i):
+            print(" ", end="")
+         
+        # Print asterisks for the current row
+        for k in range(1, 2*i):
+            print("*", end="")
+        
+def print_space(space):
+    if space > 0:
+        print(" ", end="")
+        print_space(space - 1)
+ 
+def print_star(star):
+    if star > 0:
+        print("* ", end="")
+        print_star(star - 1)
+ 
+def print_pyramid(n, current_row=1):
+    if current_row > n:
+        return
+ 
+    spaces = n - current_row
+    stars = 2 * current_row - 1
+ 
+  
+    print_space(spaces)
+ 
+    print_star(stars)
+ 
+   
+    print()
+ 
+   
+    print_pyramid(n, current_row + 1)
+ 
+
+n = 5
+ 
+
+print_pyramid(n)
